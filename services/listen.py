df0904344e4115292497db602e51b2d20cbb219c
mamachanko <max@rootswiseyouths.com>
2015-07-03 16:46:34 +0200

    follow up on past tense event name

--- services/listen.py
+++ services/listen.py
@@ -4,5 +4,5 @@ import lymph
 class Listen(lymph.Interface):
 
     @lymph.event('greeted')
-    def on_greeting(self, event):
+    def on_greeted(self, event):
         print('Somebody greeted %s' % event['name'])
