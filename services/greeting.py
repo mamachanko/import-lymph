0b542cf3a899e575b0297b87ffe53317ed72e3e7
Dushyant Rijhwani <dushyantrijhwani@gmail.com>
2015-07-17 14:28:41 +0200

    Adding docstring to the greet method of the Greeting interface

--- services/greeting.py
+++ services/greeting.py
@@ -5,6 +5,8 @@ class Greeting(lymph.Interface):
 
     @lymph.rpc()
     def greet(self, name):
+        """ Returns a greeting for a provided name.
+        """
         print('Saying hi to %s' % name)
         self.emit('greeted', {'name': name})
         return u'Hi, %s!' % name
